406710169544ee4ab8144dd7c504c9466f3a5271
ABDUL GAFFAR <gaffarsk273@gmail.com>
2025-11-02 23:35:24 +0530

    worjing cahnges by ags

--- examples/startServer.ts
+++
@@ -1,5 +0,0 @@
-import { TakossAPIServer } from '../src/api/server';
-
-const server = new TakossAPIServer(3000);
-
-server.start().catch(console.error);
--- frontend/src/hooks/index.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Hooks barrel export
- */
-export { useAuth } from './useAuth';
-export { useProjects } from './useProjects';
-export { useTheme } from './useTheme';
